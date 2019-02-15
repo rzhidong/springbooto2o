39a86a79256f4d7b2aeb8496ab5d27932710e99f
rzhidong <rzhidong@126.com>
2019-02-15 12:49:34 +0800

    验证码迁移(由于web.xml不生效了，需要在MvcConfig配置Kaptcha验证码[ServletServletRegistrationBean])

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 3. 配置service,迁移dto、enums、exception、service、cache
 4. 配置web,迁移web
 5. 前端迁移，修改静态文件addResourceHandlers配置
+6. 验证码迁移(由于web.xml不生效了，需要在MvcConfig配置Kaptcha验证码[ServletServletRegistrationBean])
--- src/main/java/com/o2o/config/web/MvcConfig.java
+++ src/main/java/com/o2o/config/web/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.o2o.config.web;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.google.code.kaptcha.servlet.KaptchaServlet;
 
 /**
  * 启Mvc,自动注入spring容器。 WebMvcConfigurerAdapter：配置视图解析器
@@ -88,6 +92,53 @@ public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		multipartResolver.setMaxUploadSize(20971520);
 		multipartResolver.setMaxInMemorySize(20971520);
 		return multipartResolver;
+	}
+	
+	//kaptcha配置
+	
+	@Value("${kaptcha.border}")
+	private String border;
+
+	@Value("${kaptcha.textproducer.font.color}")
+	private String fcolor;
+
+	@Value("${kaptcha.image.width}")
+	private String width;
+
+	@Value("${kaptcha.textproducer.char.string}")
+	private String cString;
+
+	@Value("${kaptcha.image.height}")
+	private String height;
+
+	@Value("${kaptcha.textproducer.font.size}")
+	private String fsize;
+
+	@Value("${kaptcha.noise.color}")
+	private String nColor;
+
+	@Value("${kaptcha.textproducer.char.length}")
+	private String clength;
+
+	@Value("${kaptcha.textproducer.font.names}")
+	private String fnames;
+	
+	/**
+	 * 由于web.xml不生效了，需要在这里配置Kaptcha验证码Servlet
+	 */
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean servlet = new ServletRegistrationBean(new KaptchaServlet(), "/Kaptcha");
+		servlet.addInitParameter("kaptcha.border", border);// 无边框
+		servlet.addInitParameter("kaptcha.textproducer.font.color", fcolor); // 字体颜色
+		servlet.addInitParameter("kaptcha.image.width", width);// 图片宽度
+		servlet.addInitParameter("kaptcha.textproducer.char.string", cString);// 使用哪些字符生成验证码
+		servlet.addInitParameter("kaptcha.image.height", height);// 图片高度
+		servlet.addInitParameter("kaptcha.textproducer.font.size", fsize);// 字体大小
+		servlet.addInitParameter("kaptcha.noise.color", nColor);// 干扰线的颜色
+		servlet.addInitParameter("kaptcha.textproducer.char.length", clength);// 字符个数
+		servlet.addInitParameter("kaptcha.textproducer.font.names", fnames);// 字体
+		return servlet;
 	}
 
 }
